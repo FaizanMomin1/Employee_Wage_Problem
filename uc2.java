30e074ed5b2bf575243281fd72c4de5a97799d10
“FaizanMomin1” <faizanmomin8991@gmail.com>
2023-04-15 10:49:47 +0530

    daily employee wage

---
+++ uc2.java
@@ -0,0 +1,9 @@
+public class uc2 {
+    public static void main(String[] args) {
+        int wage_per_hr = 20;
+        int full_day_hr = 8;
+        int daily_emplo_wage = wage_per_hr * full_day_hr;
+        System.out.println(daily_emplo_wage);
+
+    }
+}
