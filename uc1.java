dfa915a6e8711f5decc4f38b7b323638ad71323c
“FaizanMomin1” <faizanmomin8991@gmail.com>
2023-04-15 10:38:50 +0530

    employee present or absent using random

---
+++ uc1.java
@@ -0,0 +1,13 @@
+public class uc1 {
+    public static void main(String[] args) {
+        System.out.println("****Employee Wage Computation Project *****");
+        long random = Math.round(Math.random());
+        if (random == 1) {
+            System.out.println("employee is present");
+        }
+        else {
+            System.out.println("employee is absent");
+        }
+
+    }
+}
